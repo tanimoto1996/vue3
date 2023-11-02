4a776df9441576aacdfa4c980370e251d73a0c00
tanimoto <yumenikki2@gmail.com>
2023-11-02 19:55:39 +0900

    構文、riactiveを活用した処理

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,47 +1,47 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, reactive, computed } from "vue";
+
+// 現在時刻
+const now = new Date();
+const nowStr = now.toLocaleTimeString();
+let timeStr = nowStr;
+const timeStrRef = ref(nowStr);
+
+function changeTime(): void {
+  const newTime = new Date();
+  const newTimeStr = newTime.toLocaleTimeString();
+  timeStrRef.value = newTimeStr;
+}
+setInterval(changeTime, 1000);
+
+// 円の計算
+
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10)
+});
+
+const area = computed(
+  ():number => {
+    return data.radius * data.radius * data.PI;
+  }
+);
+
+setInterval(
+  ():void => {
+    data.radius = Math.round(Math.random() * 10);
+  },
+  1000
+);
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>時刻</h1>
+  <p>現在時刻： {{ timeStr }}</p>
+  <p>現在時刻(ref)： {{ timeStrRef }}</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h1>円周率</h1>
+  <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると、{{ area }}になる</p>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
