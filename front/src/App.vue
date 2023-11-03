52f32acaee9d9095147b4af78fd5e748a9cf1341
tanimoto <yumenikki2@gmail.com>
2023-11-04 03:34:14 +0900

    v-onディレクティブ記述

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import {reactive, ref} from "vue"
 
-const url = ref("https://vuejs.org/");
-const isSendButtonDisablesd = ref(true);
-const widthOrHeight = ref("height"); // widthに変更される可能性があるのでref関数が必要。
-const widthOrHeightValue = ref(100);
+// クリックイベント
+const randValue = ref("まだです。");
+const onButtonClick = (): void => {
+  const rand = Math.round(Math.round(Math.random() * 10));
+randValue.value = String(rand);
+}
 
+// ホバーイベント
 const imgAttributes = reactive({
   src: "/images/logo.svg",
   alt: "Vueのロゴ",
@@ -13,43 +16,63 @@ const imgAttributes = reactive({
   height: 75
 })
 
-const msg = ref("こんちわ―世界");
-const isTextColorRed = ref(true);
-const isBgColorBlue = ref(false);
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMousemove = (event: MouseEvent): void => {
+  mousePointerX.value = event.offsetX;
+  mousePointerY.value = event.offsetY;
+}
 
+// prevent修飾子
+const msg = ref("未送信");
+const onFormSubmit = (): void => {
+  msg.value = "送信されました。"
+}
+
+// keyEvent
+const keyMsg = ref("まだ");
+const onEnterKey = (): void => {
+  keyMsg.value = "エンターキーが押下されました";
+}
+
+const onRightButtonClick = (): void => {
+  keyMsg.value = "右クリックされました。";
+}
+
+const onShiftClick = (): void => {
+  keyMsg.value = "右クリックされました。";
+}
 
 </script>
 
 <template>
-  <!-- v-bindディレクティブ -->
-  <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
-  <p><a :href="url" target="_blank">Vue.jsのサイト（省略系）</a></p>  <!-- 省略形は基本的に使わない。 -->
-  <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドページ</a></p>
-  <p><a href="{{ url }}">{{ url }}</a></p>  <!-- もちろん、属性の値に変数を使用することは不可なので、ディレクティブを使用する -->
+  <!-- クリックイベント -->
+  <section>
+    <button v-on:click="onButtonClick">クリック！</button>
+    <p>クリックの結果：{{ randValue }}</p>
+  </section>
 
-  <!-- 値がない属性に対してのv-bind -->
-  <p><button type="button" v-bind:disabled="isSendButtonDisablesd">送信</button></p>
+  <!-- ホバーイベント -->
+  <section>
+    <img v-bind:="imgAttributes" v-on:mousemove="onImgMousemove">
+    <p>ポインタの位置： X={{ mousePointerX }}; y={{ mousePointerY }}</p>
+  </section>
 
-  <!-- テンプレート変数を使用したv-bindの使用例 -->
-  <p><img src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue" alt="VueLogo"></p>
+  <!-- prevent修飾子 -->
+  <form action="#" v-on:submit.prevent="onFormSubmit">
+    <input type="text" required>
+    <button type="submit">送信</button>
+  </form>
+  <p>{{ msg }}</p>
 
-  <!-- templateの属性の方が優先される -->
-  <p><img v-bind="imgAttributes"></p> <!-- <img src="/images/logo.svg" alt="Vueのロゴ" width="75" height="75"> -->
-  <p><img v-bind="imgAttributes" alt="ロゴです～～"></p><!-- <img src="/images/logo.svg" alt="ロゴです～～" width="75" height="75"> -->
-
-  <!-- v-bind:classで値にboolを指定することで、classの表示有無を記述できる -->
-  <p v-bind:class="{ textColorRed: isTextColorRed }">
-    {{ msg }}
-  </p>
-
-  <p v-bind:class="{ textColorRed: isBgColorBlue }">
-    {{ msg }}
-  </p>
+  <!-- keyEvent -->
+  <input type="text" v-on:keydown.enter="onEnterKey"><br>
+  <button v-on:click.right="onRightButtonClick">右クリック</button><br>
+  <button v-on:click.shift="onShiftClick">シフトを押しながらクリックする</button>
+  <p>{{ keyMsg }}</p>
 </template>
 
 <style scoped>
-.textColorRed {
-  color: red;
-}
+
 
 </style>
