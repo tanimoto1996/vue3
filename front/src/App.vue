ffcac4a2539e759ec3d57d4c4769e9ad92d54791
tanimoto <yumenikki2@gmail.com>
2023-11-04 04:12:18 +0900

    v-modelディレクトリ記述

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,75 +1,74 @@
 <script setup lang="ts">
 import {reactive, ref} from "vue"
 
-// クリックイベント
-const randValue = ref("まだです。");
-const onButtonClick = (): void => {
-  const rand = Math.round(Math.round(Math.random() * 10));
-randValue.value = String(rand);
-}
+// 双方向バインディング
+const inputTextarea = ref("テキストエリアへの入力文字");
+const memberType = ref(1);
+const memberTypeSelect = ref(1);
+const isAgreed = ref(false);
+const isAgreed01 = ref(0);
+const selectedOS = ref([]);
+const selectedOSSelect = ref([]);
 
-// ホバーイベント
-const imgAttributes = reactive({
-  src: "/images/logo.svg",
-  alt: "Vueのロゴ",
-  width: 75,
-  height: 75
-})
-
-const mousePointerX = ref(0);
-const mousePointerY = ref(0);
-const onImgMousemove = (event: MouseEvent): void => {
-  mousePointerX.value = event.offsetX;
-  mousePointerY.value = event.offsetY;
-}
-
-// prevent修飾子
-const msg = ref("未送信");
-const onFormSubmit = (): void => {
-  msg.value = "送信されました。"
-}
-
-// keyEvent
-const keyMsg = ref("まだ");
-const onEnterKey = (): void => {
-  keyMsg.value = "エンターキーが押下されました";
-}
-
-const onRightButtonClick = (): void => {
-  keyMsg.value = "右クリックされました。";
-}
-
-const onShiftClick = (): void => {
-  keyMsg.value = "右クリックされました。";
-}
 
 </script>
 
 <template>
-  <!-- クリックイベント -->
+  <!-- テキストエリア -->
+  <textarea v-model="inputTextarea"></textarea>
+  <br>
+
+  <!-- ラジオボタン -->
   <section>
-    <button v-on:click="onButtonClick">クリック！</button>
-    <p>クリックの結果：{{ randValue }}</p>
+    <label for=""><input type="radio" name="memberType" value="1" v-model="memberType">通常会員</label>
+    <label for=""><input type="radio" name="memberType" value="2" v-model="memberType">特別会員</label>
+    <label for=""><input type="radio" name="memberType" value="3" v-model="memberType">有料会員</label>
+    <br>
+    <p>選択されたボタン: {{ memberType }}</p>
+  </section>
+  <br>
+
+  <section>
+    <select v-model="memberTypeSelect">
+      <option value="1">通常会員</option>
+      <option value="2">特別会員</option>
+      <option value="3">有料会員</option>
+    </select>
+    <br>
+    <p>選択されたリスト：{{ memberTypeSelect }}</p>
+  </section>
+  <br>
+
+  <section>
+    <label for=""><input type="checkbox" v-model="isAgreed">同意する</label>
+    <p>同意の結果：{{ isAgreed }}</p>
+  </section>
+  <br>
+
+  <section>
+    <label for=""><input type="checkbox" v-model="selectedOS" value="1">macOS</label>
+    <label for=""><input type="checkbox" v-model="selectedOS" value="2">Windows</label>
+    <label for=""><input type="checkbox" v-model="selectedOS" value="3">Linux</label>
+    <label for=""><input type="checkbox" v-model="selectedOS" value="4">iOS</label>
+    <label for=""><input type="checkbox" v-model="selectedOS" value="5">Android</label>
+    <p>選択されたOS：{{ selectedOS }}</p>
+  </section>
+  <br>
+
+  <section>
+    <select v-model="selectedOSSelect" multiple>
+      <option value="1">macOS</option>
+      <option value="2">Windows</option>
+      <option value="3">Linux</option>
+      <option value="4">iOS</option>
+      <option value="5">Android</option>
+    </select>
+    <p>選択されたOS：{{ selectedOSSelect }}</p>
   </section>
 
-  <!-- ホバーイベント -->
-  <section>
-    <img v-bind:="imgAttributes" v-on:mousemove="onImgMousemove">
-    <p>ポインタの位置： X={{ mousePointerX }}; y={{ mousePointerY }}</p>
-  </section>
+  <!-- fileはv-modelを使えない -->
 
-  <!-- prevent修飾子 -->
-  <form action="#" v-on:submit.prevent="onFormSubmit">
-    <input type="text" required>
-    <button type="submit">送信</button>
-  </form>
-  <p>{{ msg }}</p>
-
-  <!-- keyEvent -->
-  <input type="text" v-on:keydown.enter="onEnterKey"><br>
-  <button v-on:click.right="onRightButtonClick">右クリック</button><br>
-  <button v-on:click.shift="onShiftClick">シフトを押しながらクリックする</button>
-  <p>{{ keyMsg }}</p>
+ 
 </template>
 
 <style scoped>
