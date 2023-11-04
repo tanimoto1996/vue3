18a3d93e85921c75aa3f1d6307ab1f9e2511067a
tanimoto <yumenikki2@gmail.com>
2023-11-04 16:41:51 +0900

    v-forディレクティブ

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,48 +1,68 @@
 <script setup lang="ts">
 import {reactive, ref, computed} from "vue"
 
-const randomNumber = computed(
-  (): number => {
-    return Math.round(Math.random() * 100);
-  }
-);
+// 単純配列のv-for
+const cocktailListInit: string[] = ["ホワイトレディ", "ブルーハワイ", "ニューヨーク"];
+const cocktailList = ref(cocktailListInit);
 
-const randomBool = computed(
-  (): Boolean => {
-    const randomNum = Math.round(Math.random() * 150);
-    return 100 < randomNum;
-  }
-)
+// MAP関数でのv-for
+const mapListInit = new Map<number, string>();
+mapListInit.set(2345, "ホワイトレディ");
+mapListInit.set(3345, "ブラックレディ");
+mapListInit.set(4345, "ピンクレディ");
+const mapList = ref(mapListInit);
+
+// typecriptのinterfaceを使用した型宣言
+const faceList: face[] = [
+  { id: 2345, name: "ホワイトレディ", price: 1200 },
+  { id: 3345, name: "ブラックレディ", price: 2200 },
+  { id: 4345, name: "ピンクレディ", price: 3200 },
+  { id: 5345, name: "ブラウンレディ", price: 4200 },
+];
+
+interface face {
+  id: number;
+  name: string;
+  price: number;
+}
+
 
 </script>
 
 <template>
-  <p>
-    点数は{{ randomNumber }}点で
-    <span v-if="randomNumber >= 80">優です。</span>
-    <span v-else-if="randomNumber >= 70">良です。</span>
-    <span v-else-if="randomNumber >= 60">可です。</span>
-    <span v-else>不可です。</span>
-  </p>
-  <br>
+  <!-- 単純配列のv-for -->
+  <ul>
+    <li v-for="cocktailName in cocktailList" v-bind:key="cocktailName">
+      {{ cocktailName }}
+    </li>
+  </ul>
 
-  <!--v-if-->
-  <section>
-    ■v-ifを使用する
-    <p v-if="randomBool">
-      条件に合致したので表示
-    </p>
-  </section>
-  <br>
+  <ul>
+    <li v-for="(cocktailName, index) in cocktailList" v-bind:key="cocktailName">
+      {{ cocktailName }}（インデックス{{ index }}）
+    </li>
+  </ul>
 
-  <!-- <p style="display: none;"> 条件に合致したので表示 </p> -->
-  <!-- 非表示はにするだけは処理が軽い -->
-  <section>
-    ■v-showを使用する
-    <p v-show="randomBool">
-      条件に合致したので表示
-    </p>
-  </section>
+  <!-- MAP関数でのv-for -->
+  <ul>
+    <li v-for="[id, name] in mapList" v-bind:key="id">
+      IDが{{ id }}のカクテルは{{ name }}です！
+    </li>
+  </ul>
+
+  <!-- typecriptのinterfaceを使用した型宣言 -->
+  <ul>
+    <li v-for="item in faceList" v-bind:key="item.id">
+      {{ item.name }}の値段は{{ item.price }}円で～す（{{ item.id }}）
+    </li>
+  </ul>
+
+  <!-- セレクトボックスで年数を表示する -->
+  <select>
+    <option v-for="year in 60" v-bind:key="year" v-bind:value="year + 1965">
+      {{ year + 1965 }}年
+    </option>
+  </select>
 </template>
 
 <style scoped>
